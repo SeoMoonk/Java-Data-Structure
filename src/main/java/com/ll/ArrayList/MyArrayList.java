0e914840ec55db5ed6ce7e4fdc57e026c0ce53cd
SeoMoonk <sunghoon1216@naver.com>
2023-04-19 14:25:05 +0900

    MyArrayList_Settings

---
+++ src/main/java/com/ll/ArrayList/MyArrayList.java
@@ -0,0 +1,46 @@
+package com.ll.ArrayList;
+
+import java.util.ArrayList;
+
+public class MyArrayList {
+
+    public static void main(String[] args) { }
+
+
+
+    boolean add() {
+
+
+        return true;
+    }
+
+    void remove() {
+
+    }
+
+    void size() {
+
+    }
+
+
+    void get() {
+
+    }
+
+    void contains() {
+
+    }
+
+    void indexOf() {
+
+    }
+
+    void isEmpty() {
+
+    }
+
+    void clear() {
+
+    }
+
+}
